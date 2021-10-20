d081973d744e44d0c62a065ad8290433f26ed7b1
paulinemarg <paulinemargossian@gmail.com>
2021-10-20 14:06:44 +0200

    added localhost:4200 as allowedOrigin

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const mongoose = require('mongoose'),
 
 const cors = require('cors');
 
-let allowedOrigins = ['http://localhost:8080','https://backend-myflix.herokuapp.com/', 'http://localhost:1234', 'https://paulinemarg-myflix.netlify.app'];
+let allowedOrigins = ['http://localhost:8080','https://backend-myflix.herokuapp.com/', 'http://localhost:1234', 'https://paulinemarg-myflix.netlify.app', 'http://localhost:4200'];
 app.use(cors({
   origin: (origin, callback) => {
     if (!origin) return callback(null, true);
