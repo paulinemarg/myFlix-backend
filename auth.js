9956ee51b63d2dcf7cf243ab488e0f919a8dbbbe
paulinemarg <paulinemargossian@gmail.com>
2021-10-27 16:18:18 +0200

    added commenting

--- auth.js
+++ auth.js
@@ -14,8 +14,11 @@ let generateJWTToken = (user) => {
   });
 }
 
-
-/* POST login. */
+/**
+ * POST login.
+ * @param {*} router
+ * @returns {object} Token, User
+ */
 module.exports = (router) => {
   router.post('/login', (req, res) => {
     passport.authenticate('local', { session: false }, (error, user, info) => {
